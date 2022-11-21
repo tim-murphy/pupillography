30b66f3b20ec345fac7053d59ca1a82b291967a4
Tim Murphy <murphyti@deakin.edu.au>
2022-11-21 14:29:24 +1100

    detect socket close and exit cleanly

--- dummy_gp3.py
+++ dummy_gp3.py
@@ -1,3 +1,4 @@
+import errno
 import random
 import socket
 import sys
@@ -6,38 +7,47 @@ import time
 import pupillography
 
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        print("Waiting for connection...")
+        sock.bind(pupillography.ADDRESS)
+        sock.listen()
+        conn, addr = sock.accept()
+        print("Connected.")
 
-    print("Waiting for connection...")
-    sock.bind(pupillography.ADDRESS)
-    sock.listen()
-    conn, addr = sock.accept()
-    print("Connected.")
+        with conn:
+            last_right = 5.00
+            last_left = 4.80
+            last_time = 0.00
+            change_mm = 0.10
 
-    with conn:
-        last_right = 5.00
-        last_left = 4.80
-        last_time = 0.00
-        change_mm = 0.10
+            keepRunning = True
+            print("Sending data to ", pupillography.HOST, ":", pupillography.PORT, "...", sep="")
 
-        while True:
-            outData = '<REC TIME="' + str(last_time) + '" LPMM="' + str(last_left) + '" LPMMV="1" RPMM="' + str(last_right) + '" RPMMV="1" />\r\n'
-            print(outData, end="")
-            conn.send(str.encode(outData))
+            while keepRunning:
+                outData = '<REC TIME="' + str(last_time) + '" LPMM="' + str(last_left) + '" LPMMV="1" RPMM="' + str(last_right) + '" RPMMV="1" />\r\n'
 
-            # update data
-            last_right += (change_mm if random.getrandbits(1) else -change_mm)
-            last_left += (change_mm if random.getrandbits(1) else -change_mm)
-            last_time += 1.0 / pupillography.GAZEPOINT_REFRESH
+                try:
+                    conn.send(str.encode(outData))
+                except socket.error as e:
+                    if e.errno == errno.WSAECONNRESET:
+                        print("Pupillography program has stopped.")
+                        keepRunning = False
+                    else:
+                        raise
 
-            # keep in bounds
-            last_right = max(last_right, pupillography.PUPIL_MIN_SIZE_MM)
-            last_right = min(last_right, pupillography.PUPIL_MAX_SIZE_MM)
-            last_left = max(last_left, pupillography.PUPIL_MIN_SIZE_MM)
-            last_left = min(last_left, pupillography.PUPIL_MAX_SIZE_MM)
+                # update data
+                last_right += (change_mm if random.getrandbits(1) else -change_mm)
+                last_left += (change_mm if random.getrandbits(1) else -change_mm)
+                last_time += 1.0 / pupillography.GAZEPOINT_REFRESH
 
-            time.sleep(1.0 / pupillography.GAZEPOINT_REFRESH)
+                # keep in bounds
+                last_right = max(last_right, pupillography.PUPIL_MIN_SIZE_MM)
+                last_right = min(last_right, pupillography.PUPIL_MAX_SIZE_MM)
+                last_left = max(last_left, pupillography.PUPIL_MIN_SIZE_MM)
+                last_left = min(last_left, pupillography.PUPIL_MAX_SIZE_MM)
 
-    sock.close()
+                time.sleep(1.0 / pupillography.GAZEPOINT_REFRESH)
+
+    print("Finished! Have a nice day.")
 
 # EOF
